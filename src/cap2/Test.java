f14413d0555dc76e48f408b8ee8ebe668632120b
leandrorochaadm <leandrorochaadm@gmail.com>
2020-07-26 21:58:09 -0400

    corrigindo caracteres UTF-8

--- src/cap2/Test.java
+++ src/cap2/Test.java
@@ -7,25 +7,25 @@ public class Test {
 //	private static final long n = Long.parseLong(System.console().readLine());
 
 	public static void main(String[] args) {
-		System.out.println("Digite o n˙mero para progress„o");
+		System.out.println("Digite o n√∫mero para progress√£o");
 		//System.out.println(input.nextLong());
 		long n = new Scanner(System.in).nextLong();
 	/*	
-		System.out.println("*** Progress„o simples");
+		System.out.println("*** Progress√£o simples");
 		Progression pr = new Progression();
 		pr.printProgression(n);
 		*/
 		
-		System.out.println("*** Progress„o arimetica");
+		System.out.println("*** Progress√£o arimetica");
 		Progression pa = new ArithProgression();
 		pa.printProgression(n);
 		
 
-		System.out.println("*** Progress„o geometrica");
+		System.out.println("*** Progress√£o geometrica");
 		Progression pg = new GeomProgression();
 		pg.printProgression(n);
 
-		System.out.println("*** Progress„o Fiboncci");
+		System.out.println("*** Progress√£o Fiboncci");
 		Progression pf = new FibonacciProgression();
 		pf.printProgression(n);
 	}
